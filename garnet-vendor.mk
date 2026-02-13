352751d8caa085197e2c9d8791ba4cfd66dfd3b3
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2026-02-13 09:09:53 -0300

    garnet: Allow to enable/disable ESIM (eUICC)
    Change-Id: I4ad731dbfa94755e32f8627dc4f373dc3316fbf5
    Signed-off-by: AdarshGrewal <adarshgrewal@gmail.com>

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -1857,6 +1857,7 @@ PRODUCT_PACKAGES += \
     uimgbamanagerlibrary \
     uimservicelibrary \
     com.android.hotwordenrollment.common.util \
+    mirilhook \
     qcrilhook \
     vendor.qti.hardware.alarm-V1.0-java \
     vendor.qti.ims.connection-V1.0-java \
