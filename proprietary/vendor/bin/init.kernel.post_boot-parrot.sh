802f8f2654977d3c31abec1fbccf1249b24f6b42
achu <achu.github@gmail.com>
2026-05-12 07:17:57 +0000

    parrot: reduce input boost duration to 150ms
    Lower input_boost_ms from 190 to 150 to improve thermal efficiency and
    battery life while maintaining responsive touch and stable gaming
    performance.
    
    Signed-off-by: zerotwo-alt <amruthv780@gmail.com>

--- proprietary/vendor/bin/init.kernel.post_boot-parrot.sh
+++ proprietary/vendor/bin/init.kernel.post_boot-parrot.sh
@@ -85,7 +85,7 @@ echo 0 > /sys/devices/system/cpu/cpufreq/policy4/walt/pl
 
 # configure input boost settings
 echo 1110000 0 0 0 0 0 0 0 > /proc/sys/walt/input_boost/input_boost_freq
-echo 190 > /proc/sys/walt/input_boost/input_boost_ms
+echo 150 > /proc/sys/walt/input_boost/input_boost_ms
 
 #MIUI ADD: Performance_BoostFramework
 echo 1958400 0 0 0 2400000 0 0 0 > /proc/sys/walt/input_boost/powerkey_input_boost_freq
